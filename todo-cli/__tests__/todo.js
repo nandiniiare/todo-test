5e9caf521332d12ead6f2fe004cbea3326b00a14
Nandini <145583472+NANDINIPASI@users.noreply.github.com>
2023-12-13 00:48:19 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -16,66 +16,76 @@ describe("Tests for functions in todo.js", function () {
     await db.sequelize.sync({ force: true });
   });
 
-  test("Test to add a todo", async () => {
-    const todo = await db.Todo.addTask({
-      title: "Sample todo",
-      dueDate: getJSDate(3),
-      completed: false,
-    });
-    expect(todo.id).toBeDefined();
-    // Additional assertions based on your implementation
+ test("Test to add a todo", async () => {
+  const todo = await db.Todo.addTask({
+    title: "Sample todo",
+    dueDate: getJSDate(3),
+    completed: false,
   });
 
-  test("Test to mark a todo as complete", async () => {
-    const todo = await db.Todo.addTask({
-      title: "Sample todo",
-      dueDate: getJSDate(3),
-      completed: false,
-    });
+  expect(todo.id).toBeDefined();
+  expect(todo.title).toBe("Sample todo");
+  expect(todo.dueDate).toEqual(getJSDate(3));
+  expect(todo.completed).toBe(false);
+});
 
-    await db.Todo.markAsComplete(todo.id);
 
-    const updatedTodo = await db.Todo.findByPk(todo.id);
-    expect(updatedTodo.completed).toBe(true);
-    // Additional assertions based on your implementation
+ test("Test to mark a todo as complete", async () => {
+  const todo = await db.Todo.addTask({
+    title: "Sample todo",
+    dueDate: getJSDate(3),
+    completed: false,
   });
 
-  test("Test to retrieve overdue items", async () => {
-    const overdueTodo = await db.Todo.addTask({
-      title: "Overdue todo",
-      dueDate: getJSDate(-1),
-      completed: false,
-    });
+  await db.Todo.markAsComplete(todo.id);
 
-    const overdueItems = await db.Todo.overdue();
-    expect(overdueItems.length).toBe(1);
-    expect(overdueItems[0].id).toBe(overdueTodo.id);
-    // Additional assertions based on your implementation
+  const updatedTodo = await db.Todo.findByPk(todo.id);
+  expect(updatedTodo.completed).toBe(true);
+  expect(updatedTodo.completedAt).toBeDefined();
+});
+
+
+ test("Test to retrieve overdue items", async () => {
+  const overdueTodo = await db.Todo.addTask({
+    title: "Overdue todo",
+    dueDate: getJSDate(-1),
+    completed: false,
   });
 
-  test("Test to retrieve due today items", async () => {
-    const dueTodayTodo = await db.Todo.addTask({
-      title: "Due today todo",
-      dueDate: getJSDate(0),
-      completed: false,
-    });
+  const overdueItems = await db.Todo.overdue();
+  expect(overdueItems.length).toBe(1);
+  expect(overdueItems[0].id).toBe(overdueTodo.id);
+  expect(overdueItems[0].dueDate).toBeLessThan(new Date()); // Due date is in the past
+});
 
-    const dueTodayItems = await db.Todo.dueToday();
-    expect(dueTodayItems.length).toBe(1);
-    expect(dueTodayItems[0].id).toBe(dueTodayTodo.id);
-    // Additional assertions based on your implementation
+
+test("Test to retrieve due today items", async () => {
+  const dueTodayTodo = await db.Todo.addTask({
+    title: "Due today todo",
+    dueDate: getJSDate(0),
+    completed: false,
   });
 
-  test("Test to retrieve due later items", async () => {
-    const dueLaterTodo = await db.Todo.addTask({
-      title: "Due later todo",
-      dueDate: getJSDate(2),
-      completed: false,
-    });
+  const dueTodayItems = await db.Todo.dueToday();
+  expect(dueTodayItems.length).toBe(1);
+  expect(dueTodayItems[0].id).toBe(dueTodayTodo.id);
+  expect(dueTodayItems[0].dueDate.toDateString()).toBe(new Date().toDateString());
+});
 
-    const dueLaterItems = await db.Todo.dueLater();
-    expect(dueLaterItems.length).toBe(1);
-    expect(dueLaterItems[0].id).toBe(dueLaterTodo.id);
+
+test("Test to retrieve due later items", async () => {
+  const dueLaterTodo = await db.Todo.addTask({
+    title: "Due later todo",
+    dueDate: getJSDate(2),
+    completed: false,
+  });
+
+  const dueLaterItems = await db.Todo.dueLater();
+  expect(dueLaterItems.length).toBe(1);
+  expect(dueLaterItems[0].id).toBe(dueLaterTodo.id);
+  expect(dueLaterItems[0].dueDate).toBeGreaterThan(new Date()); // Due date is in the future
+});
+
     // Additional assertions based on your implementation
   });
 });
